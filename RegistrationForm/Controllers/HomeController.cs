ec59382cdf6ed283819dbe5492dba5669c6f1489
Dell <Dell@DESKTOP-CHTFR60>
2023-06-21 03:09:47 -0700

    Add project files.

---
+++ RegistrationForm/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using RegistrationForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(Registration reg)
+        {
+            if(reg.FirstName !=null && reg.LastName != null && reg.Password != null && reg.ConfirmPassword !=null && reg.Email !=null)
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ RegistrationForm/Models/Registration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class Registration
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
